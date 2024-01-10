f1aa533f543d74f25e8e770b3f84ddb42622e661
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-10 12:09:09 +0530

    Implemented Header

---
+++ src/components/Main/MainComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderComponent from '../UI/Headers/HeaderComponent'
+
+const MainComponent = () => {
+    return (
+        <div>
+            <HeaderComponent />
+        </div>
+    )
+}
+
+export default MainComponent
