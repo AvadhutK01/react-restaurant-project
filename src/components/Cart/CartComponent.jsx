0d9e16a880a0db5fd2a8aa5fe89ba13348839cfd
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-11 11:57:14 +0530

    Cart item increase and decrease quantity

--- src/components/Cart/CartComponent.jsx
+++ src/components/Cart/CartComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../context/CartContext";
 
 const CartComponent = () => {
@@ -7,7 +7,19 @@ const CartComponent = () => {
 
     const meals = cartCtx.cart;
 
-    const totalAmount = meals.reduce((total, meal) => total + meal.amount, 0);
+    const [totalAmount, setTotalAmount] = useState(0);
+
+    useEffect(() => {
+        setTotalAmount(meals.reduce((total, meal) => total + meal.amount, 0));
+    }, [meals])
+
+    const incrementQuantity = (Id) => {
+        cartCtx.updateQuantityHandler(Id, '+');
+    }
+
+    const decrementQuantity = (Id) => {
+        cartCtx.updateQuantityHandler(Id, '-');
+    }
 
     return (
         <>
@@ -15,6 +27,16 @@ const CartComponent = () => {
                 {meals.map((meal) => (
                     <li key={meal.Id}>
                         <div className='ms-5'>{meal.quantity}x {meal.name}</div>
+                        <div className="custom-number-input h-10 w-32 mx-1">
+                            <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                                <button data-action="decrement" onClick={() => decrementQuantity(meal.Id)} className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                                    <span className="m-auto text-2xl font-thin">−</span>
+                                </button>
+                                <button data-action="increment" onClick={() => incrementQuantity(meal.Id)} className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                                    <span className="m-auto text-2xl font-thin">+</span>
+                                </button>
+                            </div>
+                        </div>
                         <div className='mx-5'>र{meal.amount}</div>
                     </li>
                 ))}
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 const CartContext = React.createContext({
     cart: [],
     addToCartHandler: (item) => { },
+    updateQuantityHandler: (Id, Mode) => { },
     cartCount: 0
 });
 
@@ -37,8 +38,27 @@ export const CartContextProvider = (props) => {
         }
     }
 
+    const updateQuantityHandler = (Id, mode) => {
+        setCart(cart.map((item) => {
+            if (item.Id === Id) {
+                let newQty = (mode === '+') ? item.quantity + 1 : item.quantity - 1;
+                if (newQty > 0) {
+                    return {
+                        ...item,
+                        'quantity': newQty,
+                        'amount': (item.amount / item.quantity) * newQty
+                    };
+                } else {
 
-    return <CartContext.Provider value={{ cartCount: cartCount, cart: cart, addToCartHandler: addToCartHandler }}>
+                    return null;
+                }
+            }
+            return item;
+        }).filter(Boolean));
+    };
+
+
+    return <CartContext.Provider value={{ cartCount: cartCount, cart: cart, addToCartHandler: addToCartHandler, updateQuantityHandler: updateQuantityHandler }}>
         {props.children}
     </CartContext.Provider>
 }
